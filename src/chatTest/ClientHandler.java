d56356a89b449650397236a9a9d8ace76cb3e0c7
trionis <trionis@naver.com>
2023-03-16 21:35:35 +0900

    awef

---
+++ src/chatTest/ClientHandler.java
@@ -0,0 +1,83 @@
+package chatTest;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ClientHandler implements Runnable {
+	
+	public static ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
+	private Socket socket;
+	private BufferedReader bufferedReader;
+	private BufferedWriter bufferedWriter;
+	private String clientUsername;
+	
+	public ClientHandler(Socket socket) {
+		try {
+			this.socket = socket;
+			this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+			this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			this.clientUsername = bufferedReader.readLine();
+			clientHandlers.add(this);
+			broadcastMessage( clientUsername + " 님이 입장하셨습니다.");
+		}catch(IOException e) {
+			closeEverything(socket, bufferedReader, bufferedWriter);
+		}
+	}
+	
+	@Override
+	public void run() {
+		String messageFromClient;
+		
+		while(socket.isConnected()) {
+			try {
+				messageFromClient = bufferedReader.readLine();
+				broadcastMessage(messageFromClient);
+			} catch(IOException e) {
+				closeEverything(socket, bufferedReader, bufferedWriter);
+				break;
+			}
+		}
+	}
+
+	private void broadcastMessage(String messageToSend) {
+		for (ClientHandler clientHandler : clientHandlers) {
+			try {
+				if(!clientUsername.equals(clientHandler.clientUsername)) {
+					clientHandler.bufferedWriter.write(messageToSend);
+					clientHandler.bufferedWriter.newLine();
+					clientHandler.bufferedWriter.flush();
+				}
+			} catch (IOException e) {
+				closeEverything(socket, bufferedReader, bufferedWriter);
+			}
+		}
+	}
+	
+	public void removeClientHandler() {
+		clientHandlers.remove(this);
+		broadcastMessage(clientUsername + "님이 나갔습니다.");
+	}
+	
+	private void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter) {
+		removeClientHandler();
+		try {
+			if(bufferedReader != null) {
+				bufferedReader.close();
+			}
+			if(bufferedWriter != null) {
+				bufferedWriter.close();
+			}
+			if(socket != null) {
+				socket.close();
+			}
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
---
+++ src/chatTest/Server.java
@@ -0,0 +1,49 @@
+package chatTest;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+	
+	private ServerSocket serverSocket; 
+	
+	public Server(ServerSocket serverSocket) {
+		this.serverSocket = serverSocket;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		
+		ServerSocket serverSocket = new ServerSocket(8080);
+		Server server = new Server(serverSocket);
+		server.startServer();
+	}
+	
+	public void startServer() {
+		
+		try {
+			while(!serverSocket.isClosed()) {
+				Socket clientSocket = serverSocket.accept();
+				System.out.println("클라이언트 입장");
+				ClientHandler clientHandler = new ClientHandler(clientSocket);
+				
+				Thread thread = new Thread(clientHandler);
+				thread.start();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void closeServerSocket() {
+		
+		try {
+			if(serverSocket != null) {
+				serverSocket.close();
+			}
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
