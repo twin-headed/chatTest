c8e74efe353f549f4c0348cc0df7ce7760063c1a
trionis <trionis@naver.com>
2023-03-17 15:08:15 +0900

    awefa

--- src/chatTest/Server.java
+++ src/chatTest/Server.java
@@ -14,7 +14,7 @@ public class Server {
 	
 	public static void main(String[] args) throws IOException {
 		
-		ServerSocket serverSocket = new ServerSocket(8080);
+		ServerSocket serverSocket = new ServerSocket(8081);
 		Server server = new Server(serverSocket);
 		server.startServer();
 	}
